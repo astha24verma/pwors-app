7ccdc7b1bdbc81bb51f979db68f10d7a5c0f7c2f
themanojrawat <mmkumarrawat8055@gmail.com>
2024-04-25 15:25:34 +0530

    Home changed , signup moved to getstarted , nav bar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,40 +25,45 @@ function Navbar() {
         <div className="">
           <div className="flex justify-between h-16 px-10 shadow items-center">
             <div className="flex items-center space-x-8">
-              <h1 className="text-xl lg:text-4xl font-bold cursor-pointer">Wardrobe</h1>
+              <a href='/' className="hover:text-gray-400 text-gray-300"><h1 className="text-xl lg:text-4xl font-bold cursor-pointer">Wardrobe</h1></a>
+              {userId ? (
               <div className="hidden md:flex justify-around space-x-4">
-                <a href="/dashboard" className="hover:text-indigo-600 text-gray-300">Wardrobe</a>
+                <a href="/dashboard" className="hover:text-indigo-600 text-gray-300">Closet</a>
                 <a href="#" className="hover:text-indigo-600 text-gray-300">Styling</a>
                 <a href="#" className="hover:text-indigo-600 text-gray-300">Planner</a>
                 <a href="#" className="hover:text-indigo-600 text-gray-300">Contact</a>
               </div>
+               ) : (
+                <div className="hidden md:flex justify-around space-x-4">
+                <a href="#" className="hover:text-indigo-600 text-gray-300">Product</a>
+                <a href="#" className="hover:text-indigo-600 text-gray-300">Contact</a>
+              </div>
+               )}
+
             </div>
             <div className="flex space-x-4 items-center">
-            {userId ? (
-              <Link to="/">
-                
-                <button 
-                    className="hover:text-gray-300" 
-                    onClick={() => {
-                        localStorage.removeItem('userId');
-                        console.log('Logged out');
-                        navigate('/');
-                        location.reload(); 
-                    }}
-                >
-                    Logout 
-                </button>
-              </Link>
-                      ) : (
-                        <>
-                          <Link to="/login" className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">
-                            Login
-                          </Link>
-                          <Link to="/signup" className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">
-                            Sign Up
-                          </Link>
-                        </>
-                      )}
+              {userId ? (
+                <Link to="/">
+                  
+                  <button 
+                      className="hover:text-gray-300" 
+                      onClick={() => {
+                          localStorage.removeItem('userId');
+                          console.log('Logged out');
+                          navigate('/');
+                          location.reload(); 
+                      }}
+                  >
+                      Logout 
+                  </button>
+                </Link>
+                        ) : (
+                          <>
+                            <Link to="/login" className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-500 text-sm">
+                              Login
+                            </Link>
+                          </>
+                        )}
             </div>
           </div>
         </div>
