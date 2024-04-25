d34ca485b8cbe8b3dcfabfe33b2661888f59b0d8
themanojrawat <mmkumarrawat8055@gmail.com>
2024-04-25 16:14:03 +0530

    login modify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -52,23 +52,96 @@ function Login() {
     // const user = useContext(UserContext);
     // console.log('User:', user);
     return (
-        <div className="h-screen flex justify-center">
-            <div className="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
-                <form onSubmit={handleLogin}>
-                    <div className="mb-6">
-                        <label htmlFor="username" className="block text-gray-800 font-bold">Username:</label>
-                        <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" className="w-full bg-slate-100 text-black border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
-                    </div>
+        // <div className="h-screen flex justify-center">
+        //     <div className="py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl">
+        //         <form onSubmit={handleLogin}>
+        //             <div className="mb-6">
+        //                 <label htmlFor="username" className="block text-gray-800 font-bold">Username:</label>
+        //                 <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your username" className="w-full bg-slate-100 text-black border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
+        //             </div>
 
-                    <div>
-                        <label htmlFor="password" className="block text-gray-800 font-bold">Password:</label>
-                        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" className="w-full bg-slate-100 text-black border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
-                    </div>
+        //             <div>
+        //                 <label htmlFor="password" className="block text-gray-800 font-bold">Password:</label>
+        //                 <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" className="w-full bg-slate-100 text-black border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
+        //             </div>
                     
-                    <button type="submit" className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">Login</button>
-                </form>
+        //             <button type="submit" className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">Login</button>
+        //         </form>
+        //     </div>
+        // </div>
+        <>
+            <div className=" bg-transparent flex items-center justify-center h-screen">
+    <div className="bg-white rounded-lg p-10">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img
+          className="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+          alt="Workflow"
+        />
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                Username
+              </label>
+              <div className="mt-1">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="w-full bg-gray-200 text-black border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
+                />
+              </div>
             </div>
+
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full bg-gray-200 text-black border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="/Signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Sign up
+            </a>
+          </p>
         </div>
+      </div>
+    </div>
+  </div>
+      </>
     );
 }
 
